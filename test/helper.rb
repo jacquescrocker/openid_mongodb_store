b4c651382fb1f89f01e55ca328d816db3fc0bfe7
Sam Schenkman-Moore <samsm@samsm.com>
2010-09-27 13:41:22 -0400

    Simpler tests.

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'test/unit'
+require 'mocha'
 require 'ruby-debug'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
@@ -13,13 +14,51 @@ end
 class Test::Unit::TestCase
 
   def setup
-    @store = OpenidMongodbStore::Store.new(test_database)
+    timestamp(true)
+  end
+
+  def store
+    @store ||= OpenidMongodbStore::Store.new(test_database)
+  end
+
+  def store_association(opts = {})
+    association = OpenID::Association.new(handle, secret, timestamp, lifetime, assoc_type)
+    opts.each_pair {|k,v| association.send "#{k}=", v}
+    store.store_association(server_url,association)
+  end
+
+  def server_url
+    "http://localhost:98765/"
+  end
+
+  def handle
+    "{HMAC-SHA1}{4ca0a54b}{5Sx5CQ==}"
+  end
+
+  def secret
+    OpenID::Util.from_base64("5EoS1O4V+x7VkBNEekHsavgRjbk=")
+  end
+
+  def timestamp(refresh = false)
+    @timestamp = nil if refresh
+    @timestamp ||= Time.now - 10 # 10 seconds ago
+  end
+
+  def lifetime
+    1209600 # two weeks in seconds
+  end
+
+  def assoc_type
+    "HMAC-SHA1"
+  end
+
+  def salt
+    "KuIaaq"
   end
 
   def teardown
     test_database.drop_collection 'openid_mongo_store_associations'
     test_database.drop_collection 'openid_mongo_store_nonces'
-    puts 'bye'
   end
 
 end
--- test/test_openid_mongodb_store.rb
+++ test/test_openid_mongodb_store.rb
@@ -1,7 +1,21 @@
 require 'helper'
 
 class TestOpenidMongodbStore < Test::Unit::TestCase
-  def test_something_for_real
-    @store.cleanup_associations
+  def test_store_and_retrieve_association
+    # Make sure there isn't already an association in the database
+    assert !store.get_association(server_url, handle)
+
+    # Save an assocaition
+    assert store_association
+
+    # Retrieve the association
+    assert store.get_association(server_url, handle)
+  end
+
+  def test_remove_association
+  end
+
+  def test_use_nonce # three ways to test (fail a, fail b, success/create)
+
   end
 end
